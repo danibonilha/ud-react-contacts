752b2519a4509122dd93cb288914d734d4ba615a
danibonilha <lopesbond7@students.rowan.edu>
2018-05-17 23:36:25 -0300

    Using ES6 object destructuring to clean up the code

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types';
 import escapeRegExp from 'escape-string-regexp'
-import { sortyBy } from 'sort-by';
+import sortyBy from 'sort-by';
 
 class ListContacts extends Component {
   static propTypes = {
@@ -17,14 +17,15 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
   render() {
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
 
     let showingContacts
-    if (this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      console.log(match)
-      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
     } else {
-      showingContacts = this.props.contacts
+      showingContacts = contacts
     }
 
     showingContacts.sort(sortyBy('name'))
@@ -36,7 +37,7 @@ class ListContacts extends Component {
             className='search-contacts'
             type='text'
             placeholder='Search contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
@@ -51,7 +52,7 @@ class ListContacts extends Component {
                 <p>{contact.email}</p>
               </div>
               <button
-                onClick={() => { this.props.onDeleteContact(contact) }}
+                onClick={() => { onDeleteContact(contact) }}
                 className='contact-remove'
               >
                 Remove
